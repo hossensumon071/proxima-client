6b2557cff8ce61c0ecf91ee81b9c0b9b9d97cb84
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2023-03-04 20:10:49 +0600

    patch req added

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,8 +1,13 @@
 import { useProjectsContext } from "../hooks/useProjectsContext";
 import { currencyFormatter } from "../utils/currencyFormatter";
+import ProjectForm from "./ProjectForm";
 import moment from "moment";
+import { useState } from "react";
 
 const ProjectDetails = ({ project }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isOverlayOpen, setOverlayOpen] = useState(false);
+
   const { dispatch } = useProjectsContext();
   const handleDelete = async () => {
     const res = await fetch(
@@ -17,11 +22,21 @@ const ProjectDetails = ({ project }) => {
       dispatch({ type: "DELETE_PROJECT", payload: json });
     }
   };
+
+  const handleUpdate = () => {
+    setIsModalOpen(true);
+    setOverlayOpen(true);
+  };
+
+  const handleOverlay = () => {
+    setIsModalOpen(false);
+    setOverlayOpen(false);
+  };
   return (
     <div className="project bg-slate-800 p-5 rounded-xl border border-slate-700 flex flex-col gap-5 w-[22rem] hover:bg-slate-900 transition duration-150 ease-in-out">
       <div className="top">
         <span className="text-sky-400">ID: {project._id}</span>
-        <h3 className="text-2xl font-medium truncate uppercase">
+        <h3 className="text-2xl font-medium truncate capitalize">
           {project.title}
         </h3>
         <span className="uppercase text-xs tracking-widset text-slate-500 font-small">
@@ -49,7 +64,10 @@ const ProjectDetails = ({ project }) => {
       </div>
 
       <div className="bottom flex gap-5">
-        <button className="bg-sky-400 text-slate-900 py-2 px-5 rounded shadow-xl hover:bg-sky-50 duration-300">
+        <button
+          onClick={handleUpdate}
+          className="bg-sky-400 text-slate-900 py-2 px-5 rounded shadow-xl hover:bg-sky-50 duration-300"
+        >
           Update
         </button>
         <button
@@ -58,6 +76,30 @@ const ProjectDetails = ({ project }) => {
         >
           Delete
         </button>
+      </div>
+
+      {/* OVERLAY */}
+      <div
+        onClick={handleOverlay}
+        className={`overlay fixed z-[1] h-screen w-screen bg-slate-900/50 backdrop-blur-sm top-0 left-0 right-0 bottom-0 ${
+          isOverlayOpen ? "" : "hidden"
+        }`}
+      ></div>
+
+      {/* MODAL */}
+      <div
+        className={`update-modal w-[30rem] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-800 p-10 rounded-xl shadow-xl border border-slate-700 z-[2] ${
+          isModalOpen ? "" : "hidden"
+        }`}
+      >
+        <h2 className="text-4xl font-medium text-sky-400 mb-10">
+          Update Project
+        </h2>
+        <ProjectForm
+          project={project}
+          setIsModalOpen={setIsModalOpen}
+          setOverlayOpen={setOverlayOpen}
+        />
       </div>
     </div>
   );
--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { useProjectsContext } from "../hooks/useProjectsContext";
 
-const ProjectForm = () => {
-  const [title, setTitle] = useState("");
-  const [tech, setTech] = useState("");
-  const [budget, setBudget] = useState("");
-  const [duration, setDuration] = useState("");
-  const [manager, setManager] = useState("");
-  const [dev, setDev] = useState("");
+const ProjectForm = ({project, setIsModalOpen, setOverlayOpen}) => {
+  const [title, setTitle] = useState(project ? project.title : '');
+  const [tech, setTech] = useState(project ? project.tech : "");
+  const [budget, setBudget] = useState(project ? project.budget : "");
+  const [duration, setDuration] = useState(project ? project.duration : "");
+  const [manager, setManager] = useState(project ? project.manager : "");
+  const [dev, setDev] = useState(project ? project.dev : "");
   const [error, setError] = useState(null);
   const [emptyFields, setEmptyFields] = useState([]);
 
@@ -19,6 +19,8 @@ const ProjectForm = () => {
     // Data
     const projectObj = { title, tech, budget, duration, manager, dev };
 
+   // if there is no project, send post req
+   if(!project) {
     // post req
     const res = await fetch("http://localhost:5000/api/projects", {
       method: "POST",
@@ -46,10 +48,47 @@ const ProjectForm = () => {
       emptyFields([]);
       dispatch({ type: "CREATE_PROJECT", payload: json });
     }
+
+    return;
+   }
+
+   // if there is a project send patch request
+   if(project) {
+    // send patch req
+    const res = await fetch(`http://localhost:5000/api/projects/${project._id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(projectObj),
+    });
+    const json = await res.json();
+
+    //! res.ok
+    if(!res.ok) {
+      setError(json.error);
+      setEmptyFields(json.emptyFields);
+    }
+
+    // res.ok
+    if (res.ok) {
+      setError(null);
+      setEmptyFields([]);
+
+      // dispatch
+      dispatch({type: "UPDATE_PROJECT", payload: json})
+      // close overlay and modal
+      setIsModalOpen(false);
+      setOverlayOpen(false);
+    }
+
+    return; 
+   }
+
   };
   return (
     <form onSubmit={handleSubmit} className="project-form flex flex-col gap-5">
-      <h2 className="text-4xl font-medium text-sky-400 mb-10">
+      <h2 className={`text-4xl font-medium text-sky-400 mb-10 ${project ? "hidden" : ""}`}>
         Add a new project
       </h2>
       <div className="form-control flex flex-col gap-2">
@@ -175,7 +214,7 @@ const ProjectForm = () => {
         type="submit"
         className="bg-sky-400 text-slate-900 py-3 rounded-lg hover:bg-sky-50 duration-300"
       >
-        Add Project
+        {project ? "Confirm Update" : "Add Project"}
       </button>
       {error && (
         <p className="bg-rose-500/20 rounded-lg p-5 text-rose-500 border border-rose-500">
