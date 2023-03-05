745d46f8567d4abdbe6536d4bbb788f381d667a5
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2023-03-05 12:59:37 +0600

    auth context login signup form added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import {Routes, Route} from 'react-router-dom';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
-    <div className="App bg-slate-900 text-slate-100 min-h-screen">
-      <Navbar/>
+    <div className="app bg-slate-900 text-slate-100 min-h-screen">
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!user ? <Signup /> : <Navigate to="/" />}
+        />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import {BrowserRouter} from 'react-router-dom';
-import {ProjectContextProvider} from './context/ProjectContext'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ProjectContextProvider } from "./context/ProjectContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ProjectContextProvider>
+        <AuthContextProvider>
           <App />
+        </AuthContextProvider>
       </ProjectContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
